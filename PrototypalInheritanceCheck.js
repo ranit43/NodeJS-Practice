61bd2fce718d996fc3a41d177898bf2d3b7dedf8
Ranit Debnath Akash <ranitid12@gmail.com>
2020-04-16 10:28:35 +0600

    Prototypal inheritance example. Forgot to code it before.

---
+++ PrototypalInheritanceCheck.js
@@ -0,0 +1,19 @@
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+Person.prototype.callByName = function() {
+    console.log(`Hey ${this.firstName} ${this.lastName}!`);
+};
+
+var lopa = new Person('Lopa', 'Bhowmik');
+lopa.callByName();
+
+var ranit = new Person('Ranit', 'Debnath');
+ranit.callByName();
+
+
+console.log(lopa.__proto__);
+console.log(ranit.__proto__);
+console.log(lopa.__proto__ == ranit.__proto__);
